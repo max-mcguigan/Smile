a2fa3836a1b6fbac4fdaf4ad1eb0f3511f0d562e
max mcguigan <18045@wc.school.nz>
2022-03-01 11:07:01 +1300

    add menu.html, contact.html and links

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return "hello world"
+def home():
+    return render_template("home.html")
+
+
+@app.route('/menu')
+def menu():
+    return render_template("menu.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 
 app.run(host='0.0.0.0')
