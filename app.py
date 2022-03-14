c771978c07b2a058e45f22e55643794ec0131970
max mcguigan <18045@wc.school.nz>
2022-03-15 11:09:51 +1300

    copied more code from father harford

--- app.py
+++ app.py
@@ -10,7 +10,14 @@ def home():
 
 @app.route('/menu')
 def menu():
-    return render_template("menu.html")
+    products = [
+        ["Flat white", "Definitely created in New Zealand (not in the West Island) - a classic.", "180mL", "flatwhite",
+         "4.00"],
+        ["Latte", "The New Zealand latte is larger than a flat white and has more foamy milk.", "240mL", "latte",
+         "4.00"],
+        ["Espresso", "Straight from the machine, 60mL", "including crema.", "60mL", "espresso", "3.00"],
+        ["Long black", "Hot water + espresso. 120mL.", "90mL", "longblack", "3.00"]]
+    return render_template("menu.html", products=products)
 
 
 @app.route('/contact')
