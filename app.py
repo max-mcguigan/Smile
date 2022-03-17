4cb0c45763bd53942d2a8bbaa4809152c0213692
max mcguigan <18045@wc.school.nz>
2022-03-17 14:21:33 +1300

    fix menu route

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
+import sqlite3
+from sqlite3 import Error
+
+DB_NAME = 'smile.db'
 
 app = Flask(__name__)
+
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+
+    return None
 
 
 @app.route('/')
@@ -10,14 +24,13 @@ def home():
 
 @app.route('/menu')
 def menu():
-    products = [
-        ["Flat white", "Definitely created in New Zealand (not in the West Island) - a classic.", "180mL", "flatwhite",
-         "4.00"],
-        ["Latte", "The New Zealand latte is larger than a flat white and has more foamy milk.", "240mL", "latte",
-         "4.00"],
-        ["Espresso", "Straight from the machine, 60mL", "including crema.", "60mL", "espresso", "3.00"],
-        ["Long black", "Hot water + espresso. 120mL.", "90mL", "longblack", "3.00"]]
-    return render_template("menu.html", products=products)
+    con = create_connection(DB_NAME)
+    query = "SELECT name, description, volume, price, image FROM product"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    con.close()
+    return render_template("menu.html", products=product_list)
 
 
 @app.route('/contact')
